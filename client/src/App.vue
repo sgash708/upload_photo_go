d7161d7c925f5f0a38fe9f85fd612af520e68263
sgash708 <sugash7081023@gmail.com>
2022-01-07 11:07:07 +0900

    component修正

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ControllImage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ControllImage from './components/ControllImage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ControllImage
   }
 }
 </script>
--- client/src/components/ControllImage.vue
+++ client/src/components/ControllImage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="controllImage">
     <!-- sendingEventを追加 -->
     <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions" v-on:vdropzone-sending="sendingEvent" v-on:vdropzone-removed-file="removeEvent"></vue-dropzone>
   </div>
@@ -8,10 +8,10 @@
 <script>
 import vue2Dropzone from 'vue2-dropzone'
 import 'vue2-dropzone/dist/vue2Dropzone.min.css'
-import axios from "axios"
+import axios from 'axios'
 
 export default {
-  name: 'HelloWorld',
+  name: 'ControllImage',
   data: function () {
     return {
       dropzoneOptions: {
@@ -22,34 +22,32 @@ export default {
     }
   },
   components: {
+    // ケバブケース(<vue-dropzone>)でレンダリング
     vueDropzone: vue2Dropzone
   },
   mounted: function() {
+    // mountした時に、一覧を取得する処理を追加する
     axios.get('http://localhost:8888/images').then(res => {
       res.data.forEach(res => {
-        // filename 取得
         let filename = res.path.replace('http://localhost:8888/', '')
-        // uuid 取得
         let id = filename.replace('.png', '')
-        // file オブジェクト作成
-        var file = {size: res.size, name: filename, type: "image/png", upload: {uuid: id}}
+        let file = {size: res.size, name: filename, type: "image/png", upload: {uuid: id}}
+
         // コードからformに画像をセットする
         this.$refs.myVueDropzone.manuallyAddFile(file, res.path)
       })
     }).catch(err => {
-      // 例外処理
       console.error(err)
     })
   },
   methods: {
-    // UUIDをサーバに送信し判定してもらう
-    sendingEvent: function (file, xhr, formData) {
-      // アップ時に「UUID」を付与
+    // sendingEvent UUIDをサーバに送信し判定してもらう
+    sendingEvent(file, xhr, formData) {
+      // アップロード時に「UUID」を付与
       formData.append('uuid', file.upload.uuid)
     },
-    // これで合っているのか謎
-    removeEvent: function (file) {
-    // removeEvent: function (file, error, xhr) {
+    // removeEvent 削除イベント
+    removeEvent(file) {
       axios.delete(`http://localhost:8888/images/${file.upload.uuid}`).then(res => {
         console.log(res.data)
       }).catch(err => {
@@ -58,13 +56,6 @@ export default {
     }
   }
 }
-
-// export default {
-//   name: 'HelloWorld',
-//   props: {
-//     msg: String
-//   }
-// }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
